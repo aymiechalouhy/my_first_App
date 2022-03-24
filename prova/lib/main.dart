9ff663eae98236e83226e5bda01d7ceac25650e4
Aymie Chalouhy <aymie.walid.chalouhy@gmail.com>
2022-03-24 23:50:55 +0200

    thanks message

--- prova/lib/main.dart
+++ prova/lib/main.dart
@@ -1,25 +1,44 @@
 import 'package:flutter/material.dart';
 
-void main(){
-runApp (const MyApp());
+void main() {
+  runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
-  const MyApp ({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title:"First App",
-      home: HomePage(),      
+      title: "First App",
+      home: HomePage(),
     );
   }
 }
+
 class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
+  // widget huwe l return type; build esem location; buildContext betdell flutter wen location taba3 l app
   Widget build(BuildContext context) {
-    return Container(      
-    );
+    return Scaffold(
+        appBar: AppBar(title: const Text("Helloo, Hellooo Helloo")),
+        body: Center(
+            child: Container(
+          height: 250,
+          width: 250,
+          color: Colors.blue,
+          child: const Center(
+            child: Text("thank you",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold)),
+          ),
+        )
+        )
+        );
   }
 }
+// Scaffold 3nda background white
